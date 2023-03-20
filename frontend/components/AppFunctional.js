521ddfd63da800b40eafa8437e0412afdb88a39b
Cameron <cameron.morgan@skiff.com>
2023-03-20 09:26:18 -0500

    Completed onChange and onSubmit form functionality.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,54 +1,63 @@
 import React from 'react'
+import { useState } from 'react'
 
 // Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
-const initialSteps = 0
+const initialSteps = 420
 const initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+  const [form, setForm] = useState({ email: initialEmail, message: initialMessage });
+  const [counter, setCounter] = useState(initialSteps);
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
+  // COORDINATE HELPERS
+  // ==================
+  // function getXY() {
+  //   // It it not necessary to have a state to track the coordinates.
+  //   // It's enough to know what index the "B" is at, to be able to calculate them.
+  // }
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    return "this is the xymessage"
   }
 
-  function reset() {
-    // Use this helper to reset all states to their initial values.
-  }
+  // function reset() {
+  //   // Use this helper to reset all states to their initial values.
+  // }
 
-  function getNextIndex(direction) {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
-  }
+  // function getNextIndex(direction) {
+  //   // This helper takes a direction ("left", "up", etc) and calculates what the next index
+  //   // of the "B" would be. If the move is impossible because we are at the edge of the grid,
+  //   // this helper should return the current index unchanged.
+  // }
 
-  function move(evt) {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-  }
+  // function move(evt) {
+  //   // This event handler can use the helper above to obtain a new index for the "B",
+  //   // and change any states accordingly.
+  // }
 
+  // FORM HELPERS
+  // ============
   function onChange(evt) {
-    // You will need this to update the value of the input.
+    const { value, name } = evt.target;
+    setForm({ ...form, [name]: value});
   }
 
   function onSubmit(evt) {
-    // Use a POST request to send a payload to the server.
+    evt.preventDefault();
+    setForm({ ...form, "email": initialEmail, "message": getXYMessage()});
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        {/* <h3 id="coordinates">Coordinates ({coordinates.join(", ")})</h3> */}
+        <h3 id="steps">You moved {counter} times</h3>
       </div>
       <div id="grid">
         {
@@ -60,7 +69,7 @@ export default function AppFunctional(props) {
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{form.message}</h3>
       </div>
       <div id="keypad">
         <button id="left">LEFT</button>
@@ -70,8 +79,8 @@ export default function AppFunctional(props) {
         <button id="reset">reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+        <input id="email" type="email" placeholder="type email" name="email" onChange={onChange} value={form.email}></input>
+        <input id="submit" type="submit" onClick={onSubmit}></input>
       </form>
     </div>
   )
