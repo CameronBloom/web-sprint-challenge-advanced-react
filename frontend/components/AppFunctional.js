d2a5212cd45ab0016d298c33ae329ea77d16b9a2
Cameron <cameron.morgan@skiff.com>
2023-03-20 10:15:17 -0500

    Completed getNextIndex functionality.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -11,6 +11,8 @@ export default function AppFunctional(props) {
   const [form, setForm] = useState({ email: initialEmail, message: initialMessage });
   const [counter, setCounter] = useState(initialSteps);
 
+  let currIndex = initialIndex;
+
   // COORDINATE HELPERS
   // ==================
   // function getXY() {
@@ -22,18 +24,47 @@ export default function AppFunctional(props) {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
-    return "this is the xymessage"
+    return "this is the xy message"
   }
 
-  // function reset() {
-  //   // Use this helper to reset all states to their initial values.
-  // }
+  function reset() {
+    // Use this helper to reset all states to their initial values.
+    setForm({ ...form, "email": initialEmail, "message": initialMessage });
+    setCounter(initialSteps)
+    
+  }
 
-  // function getNextIndex(direction) {
-  //   // This helper takes a direction ("left", "up", etc) and calculates what the next index
-  //   // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-  //   // this helper should return the current index unchanged.
-  // }
+  function getNextIndex(direction) {
+    // This helper takes a direction ("left", "up", etc) and calculates what the next index
+    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
+    // this helper should return the current index unchanged.
+    const { id } = direction.target;
+    switch(id) {
+      case "left":
+        if (currIndex !== 0 && currIndex !== 3 && currIndex !== 6) {
+          currIndex = currIndex - 1;
+        }
+        break;
+      case "up":
+        if (currIndex - 3 >= 0) {
+          currIndex = currIndex - 3;
+        }
+        break;
+      case "right":
+        if (currIndex !== 2 && currIndex !== 5 && currIndex !== 8) {
+          currIndex = currIndex + 1;
+        }
+        break;
+      case "down":
+        if (currIndex !== 6 && currIndex !== 7 && currIndex !== 8) {
+          currIndex = currIndex + 3;
+        }
+        break;
+      default:
+        console.log(`Invalid Argument...`)
+    }
+    console.log(`new: ${currIndex}`);
+  }
 
   // function move(evt) {
   //   // This event handler can use the helper above to obtain a new index for the "B",
@@ -52,6 +83,7 @@ export default function AppFunctional(props) {
     setForm({ ...form, "email": initialEmail, "message": getXYMessage()});
   }
 
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
@@ -62,8 +94,8 @@ export default function AppFunctional(props) {
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === initialIndex ? ' active' : ''}`}>
+              {idx === initialIndex ? 'B' : null}
             </div>
           ))
         }
@@ -72,10 +104,10 @@ export default function AppFunctional(props) {
         <h3 id="message">{form.message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
+        <button id="left" onClick={ getNextIndex }>LEFT</button>
+        <button id="up" onClick={ getNextIndex }>UP</button>
+        <button id="right" onClick={ getNextIndex }>RIGHT</button>
+        <button id="down" onClick={ getNextIndex }>DOWN</button>
         <button id="reset">reset</button>
       </div>
       <form>
