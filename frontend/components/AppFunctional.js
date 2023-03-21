bb46c247674534d7fd370b9c99b69078b0b15a16
Cameron <cameron.morgan@skiff.com>
2023-03-20 19:39:43 -0500

    Completed the 'AppFunctional' component. Refactored the 'getNextIndex' and 'move' functions.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 // Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
-const initialSteps = 420
+const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
   const [form, setForm] = useState({ email: initialEmail, message: initialMessage });
   const [counter, setCounter] = useState(initialSteps);
   const [currIndex, setCurrIndex] = useState(initialIndex);
-  const [nextIndex, setNextIndex] = useState(initialIndex);
 
   // COORDINATE HELPERS
   // ==================
@@ -50,40 +49,54 @@ export default function AppFunctional(props) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-    // const { id } = direction.target;
-    // switch(id) {
-    //   case "left":
-    //     if (currIndex !== 0 && currIndex !== 3 && currIndex !== 6) {
-    //       setNextIndex(currIndex - 1);
-    //     }
-    //     break;
-    //   case "up":
-    //     if (currIndex - 3 >= 0) {
-    //       setNextIndex(currIndex - 3);
-    //     }
-    //     break;
-    //   case "right":
-    //     if (currIndex !== 2 && currIndex !== 5 && currIndex !== 8) {
-    //       setNextIndex(currIndex + 1);
-    //     }
-    //     break;
-    //   case "down":
-    //     if (currIndex !== 6 && currIndex !== 7 && currIndex !== 8) {
-    //       setNextIndex(currIndex + 3);
-    //     }
-    //     break;
-    //   default:
-    //     console.log(`Invalid Argument...`)
-    // }
+    const { id } = direction.target;
+    switch(id) {
+      case "left":
+        if (currIndex !== 0 && currIndex !== 3 && currIndex !== 6) {
+          setCurrIndex(currIndex - 1);
+          setCounter(counter + 1)
+          setForm({ ...form, "message": ""});
+        } else {
+          setForm({ ...form, "message": "You can't go left"});
+        }
+        break;
+      case "up":
+        if (currIndex !== 0 && currIndex !== 1 && currIndex !== 2) {
+          setCurrIndex(currIndex - 3);
+          setCounter(counter + 1);
+          setForm({ ...form, "message": ""});
+        } else {
+          setForm({ ...form, "message": "You can't go up"});
+        }
+        break;
+      case "right":
+        if (currIndex !== 2 && currIndex !== 5 && currIndex !== 8) {
+          setCurrIndex(currIndex + 1);
+          setCounter(counter + 1);
+          setForm({ ...form, "message": ""});
+        } else {
+          setForm({ ...form, "message": "You can't go right"});
+        }
+        break;
+      case "down":
+        if (currIndex !== 6 && currIndex !== 7 && currIndex !== 8) {
+          setCurrIndex(currIndex + 3);
+          setCounter(counter + 1);
+          setForm({ ...form, "message": ""});
+        } else {
+          setForm({ ...form, "message": "You can't go down"});
+        }
+        break;
+      default:
+        console.log(`Invalid Argument...`)
+    }
   }
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
-    // const event = evt;
-    // getNextIndex(event)
-    // const next = nextIndex
-    // setCurrIndex(next);
+    const event = evt;
+    getNextIndex(event)
   }
 
   // FORM HELPERS
@@ -106,6 +119,7 @@ export default function AppFunctional(props) {
         <h3 id="steps">You moved {counter} times</h3>
       </div>
       <div id="grid">
+        {/* <Grid currIndex={currIndex} nextIndex={nextIndex} /> */}
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
             <div key={idx} className={`square${idx === currIndex ? ' active' : ''}`}>
